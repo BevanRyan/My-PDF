3ed1339c41224b0965a532bd65c5e323951ecf57
BevanRyan <135395660+BevanRyan@users.noreply.github.com>
2023-07-06 13:34:32 +1200

    revert

--- app.py
+++ app.py
@@ -8,22 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-@st.experimental.singleton
-def get_text_splitter():
-    return CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
 
-@st.experimental.singleton
-def get_openai_embeddings():
-    return OpenAIEmbeddings()
-
-@st.experimental.singleton
-def get_llm_model():
-    return OpenAI()
 
 def main():
     load_dotenv()
@@ -35,32 +20,37 @@ def main():
     
     # extract the text
     if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
         
-        # split into chunks
-        text_splitter = get_text_splitter()
-        chunks = text_splitter.split_text(text)
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
       
-        # create embeddings
-        embeddings = get_openai_embeddings()
-        knowledge_base = FAISS.from_texts(chunks, embeddings)
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
       
-        # show user input
-        user_question = st.text_input("Ask a question about your PDF:")
-        if user_question:
-            docs = knowledge_base.similarity_search(user_question)
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
         
-            llm = get_llm_model()
-            chain = load_qa_chain(llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=user_question)
-                print(cb)
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
            
-            st.write(response)
+        st.write(response)
+    
 
 if __name__ == '__main__':
     main()
-
