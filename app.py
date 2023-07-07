cd40350834e42329f57d4ca64f5c5b905eeffd0b
BevanRyan <135395660+BevanRyan@users.noreply.github.com>
2023-07-07 14:14:52 +1200

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 @st.cache
 def initialize_text_splitter():
     # Initialize the text splitter here
     splitter = CharacterTextSplitter()
     return splitter
 splitter = initialize_text_splitter()
-
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
-
 
 
 def main():
